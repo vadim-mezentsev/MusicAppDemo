8ec6f81ab43d0b95da761bf2cbeddecad12c679f
user <webtarget7777@mail.ru>
2020-01-14 14:31:00 +0300

    add MainTabBarController

---
+++ MusicApp/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  MusicApp
+//
+//  Created by Vadim on 14/01/2020.
+//  Copyright © 2020 Vadim Mezentsev. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+    }
+
+}
