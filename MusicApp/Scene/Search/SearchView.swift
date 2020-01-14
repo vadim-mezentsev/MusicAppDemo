8340e954fae823322a19560894954a3ee183dead
user <webtarget7777@mail.ru>
2020-01-14 18:44:10 +0300

    add SetupView, update SetupViewController

---
+++ MusicApp/Scene/Search/SearchView.swift
@@ -0,0 +1,56 @@
+//
+//  SearchView.swift
+//  MusicApp
+//
+//  Created by Vadim on 14/01/2020.
+//  Copyright © 2020 Vadim Mezentsev. All rights reserved.
+//
+
+import UIKit
+
+class SearchView: UIView {
+    
+    // MARK: - Interface properties
+
+    lazy var searchController: UISearchController = {
+        let searchController = UISearchController()
+        searchController.searchBar.placeholder = "Search"
+        return searchController
+    }()
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        return tableView
+    }()
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+
+    // MARK: - Setup view
+    
+    private func setupView() {
+        backgroundColor = .systemBackground
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        addSubview(tableView)
+        
+        tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
+    }
+    
+}
--- MusicApp/Scene/Search/SearchViewController.swift
+++ MusicApp/Scene/Search/SearchViewController.swift
@@ -8,11 +8,66 @@
 
 import UIKit
 
+struct TrackCellModel {
+    let trackName: String
+    let artistName: String
+}
+
 class SearchViewController: UIViewController {
 
+    // MARK: - View data
+    
+    var cellViewModels: [TrackCellModel] = [TrackCellModel(trackName: "Tr1", artistName: "An1"),
+                                            TrackCellModel(trackName: "Tr2", artistName: "An2")]
+    
+    // MARK: - Load view
+    
+    var searchView = SearchView()
+    override func loadView() {
+        view = searchView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setupView()
+    }
+
+    // MARK: - Interface preparation
+
+    private func setupView() {
+        navigationItem.searchController = searchView.searchController
+        navigationItem.hidesSearchBarWhenScrolling = true
+        
+        searchView.searchController.searchBar.delegate = self
+        searchView.tableView.dataSource = self
+    }
+    
+}
+
+// MARK: - UITableViewDataSource
+
+extension SearchViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellViewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellViewModel = cellViewModels[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "\(cellViewModel.trackName) \(cellViewModel.artistName)"
+        cell.imageView?.image = UIImage(systemName: "music.mic")
+        return cell
+    }
+    
+}
+
+// MARK: - UISearchBarDelegate
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
     }
 
 }
